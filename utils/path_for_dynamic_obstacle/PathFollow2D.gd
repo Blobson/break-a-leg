4d8303339172ecca8acaf54256e0aa25b5b4df2b
VasyaLel <127948581+VasyaLel@users.noreply.github.com>
2023-08-02 19:30:05 +0300

    BaseDynamicObstacle_v2

--- utils/path_for_dynamic_obstacle/PathFollow2D.gd
+++
@@ -1,8 +0,0 @@
-extends PathFollow2D
-
-var DynamicObstacle = load("res://regions/common/base_dynamic_obstacle/BaseDynamicObstacle.tscn")
-
-func _ready():
-	var e = DynamicObstacle.instantiate()
-	e.position = get_parent().position
-	add_child(e)
