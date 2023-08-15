d39585b57ee6a4119b2f9416519089f70fcc7c4a
VasyaLel <127948581+VasyaLel@users.noreply.github.com>
2023-08-15 17:09:53 +0300

    #67 разовое разрушение препятствий

--- regions/common/base_static_obstacle/base_static_obstacle.gd
+++ regions/common/base_static_obstacle/base_static_obstacle.gd
@@ -2,13 +2,17 @@ class_name BaseStaticObstacle extends Node2D
 
 @export var damage: int = 5
 
+enum ObstacleState {IDLE, ACTIVATED}
+var state: ObstacleState = ObstacleState.IDLE
+
 func _ready():
 	if $AnimationPlayer.has_animation("idle"):
 		$AnimationPlayer.play("idle")
 	$Area2D.body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node):
-	if $AnimationPlayer.has_animation("activate"):
+	if $AnimationPlayer.has_animation("activate") and state == ObstacleState.IDLE:
 		$AnimationPlayer.play("activate")  # анимация активации
+		state = ObstacleState.ACTIVATED    # смена state на активированную
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
