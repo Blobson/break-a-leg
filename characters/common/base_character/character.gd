ef88bd585e8a7ab553d2666b7e780725a540d942
VasyaLel <127948581+VasyaLel@users.noreply.github.com>
2023-08-15 10:55:01 +0300

    #23 Conditioner (#64)
    * #23_conditioner
    
    * #23 conditioner

--- characters/common/base_character/character.gd
+++ characters/common/base_character/character.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+@export var move_speed: int = 500
 @export var health: int = 30 :
 	set(value):
 		if health != value:
@@ -8,6 +8,7 @@ extends CharacterBody2D
 			health = value
 			Game.health_updated.emit(health, old_value)
 
+var input = Vector2.ZERO
 
 #Получение урона
 func take_damage(damage):
@@ -16,6 +17,11 @@ func take_damage(damage):
 		if health <= 0:
 			parachute_jump()
 
+func _physics_process(delta):
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	velocity = input * move_speed
+	move_and_slide()
 
 #Прыжок с парашютом по окончании уровня и если health = 0
 func parachute_jump():
--- regions/residential_region/static_obstacles/window_with_thorny_plant/window_with_thorny_plant_1/window_with_thorny_зlant_1.gd
+++
@@ -1,8 +0,0 @@
-class_name WindowWithThornyyPlant extends BaseStaticObstacle
-
-@export var damage: int = 5
-
-func _on_body_entered(body: Node):
-	super._on_body_entered(body)
-	if body.has_method("take_damage"):
-		body.take_damage(damage)
