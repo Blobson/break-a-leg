58c6665cb383654fcb9c1176b510375b1f0d3b56
VasyaLel <127948581+VasyaLel@users.noreply.github.com>
2023-08-26 00:00:46 +0300

    бабка с ковриком

--- regions/common/base_static_obstacle/base_static_obstacle.gd
+++ regions/common/base_static_obstacle/base_static_obstacle.gd
@@ -1,6 +1,7 @@
 class_name BaseStaticObstacle extends Node2D
 
 @export var damage: int = 5
+@export var flippable: bool = false
 
 enum ObstacleState {IDLE, ACTIVATED}
 var state: ObstacleState = ObstacleState.IDLE
@@ -11,8 +12,17 @@ func _ready():
 	$Area2D.body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node):
+	# Если препятствие может поворачиваться к игроку, то используется _deploy_to_target()
+	if flippable == true and body.name == "Courier": 
+		_deploy_to_target(body)
 	if $AnimationPlayer.has_animation("activate") and state == ObstacleState.IDLE:
 		$AnimationPlayer.play("activate")  # анимация активации
 		state = ObstacleState.ACTIVATED    # смена state на активированную
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
+
+func _deploy_to_target(body):
+	var obstacle_position = $'.'.global_transform.origin.x
+	var courier_position_x = body.get_global_position().x
+	if courier_position_x > obstacle_position:
+		$Sprite2D.flip_h = true
