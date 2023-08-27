32b8dadf2521ccf674f113165be94142b2f6304d
Bartolemy12 <127960590+Bartolemy12@users.noreply.github.com>
2023-08-27 23:50:17 +0300

    #29 Dev: Птицы летающие по горизонтали (вороны, сороки или голуби)
    #29 Dev: Птицы летающие по горизонтали (вороны, сороки или голуби)

--- regions/common/base_dynamic_obstacle/base_dynamic_obstacle.gd
+++ regions/common/base_dynamic_obstacle/base_dynamic_obstacle.gd
@@ -3,5 +3,6 @@ class_name BaseDynamicObstacle extends BaseStaticObstacle
 @onready var path_follow = get_parent()
 @export var speed = 300
 
+
 func _physics_process(delta):
 	path_follow.set_progress(path_follow.get_progress() + speed * delta)
