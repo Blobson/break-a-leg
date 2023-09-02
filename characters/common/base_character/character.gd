808021721beabe7807f350838c4ecfdceef43036
Vladimir Klimov <blobson@gmail.com>
2023-09-03 01:56:48 +0300

    #85 Выравнивание движения персонажа по тайлам

--- characters/common/base_character/character.gd
+++ characters/common/base_character/character.gd
@@ -1,16 +1,25 @@
 extends CharacterBody2D
 
+@onready var animation = $AnimationPlayer
+
+## Скорость движения
 @export var move_speed: int = 200
-@export var animation_time: float = 0.3
+
+## Длина прыжка в тайлах
+@export var jump_length: float = 1.5
+
+## Жизни персонажа
 @export var health: int = 30 :
 	set(value):
 		if health != value:
 			var old_value: int = health
 			health = value
 			Game.health_updated.emit(health, old_value)
-@onready var animation = $AnimationPlayer
-var level_width
-var level_tile_size
+		
+var level_width: int
+var level_tile_size: Vector2i
+var movement_tween: Tween
+var next_move: Vector2 = Vector2.ZERO
 
 
 ## Получение урона
@@ -23,7 +32,7 @@ func take_damage(damage):
 
 func _init():
 	Game.level_start.connect(_on_level_start)
-	
+
 
 func _ready():
 	velocity.y = -move_speed
@@ -31,14 +40,15 @@ func _ready():
 	SwipeDetector.swiped.connect(_on_swipe)
 
 
-## Ширина уровня и размер тайла
+## Запуск уровня
 func _on_level_start(width, tile_size):
+	# Сохраняем ширину уровня и размер тайла
 	level_width = width
 	level_tile_size = tile_size
 
 
 ## Движение
-func _physics_process(delta):
+func _physics_process(_delta):
 	move_and_slide()
 
 
@@ -52,27 +62,66 @@ func _unhandled_input(event):
 		move(Vector2.RIGHT)
 
 
+## Обработка свайпов
 func _on_swipe(direction: Vector2):
 	if direction in [Vector2.UP, Vector2.LEFT, Vector2.RIGHT]:
 		move(direction)
 
 
-## Обработка свайпов (tweens & animations)
+## Обработка движения персонажа
 func move(direction: Vector2):
-	var tween = get_tree().create_tween()
+	if _is_moving():
+		next_move = direction
+	else:
+		movement_tween = _start_move(direction)
+
+
+func _is_moving() -> bool:
+	return movement_tween and movement_tween.is_running()
+
+
+func _start_move(direction: Vector2) -> Tween:
+	var target_position: Vector2
 	if direction == Vector2.UP:
-		tween.tween_property(self, "position:y", position.y - level_tile_size.y * 2, animation_time)
-		animation.play("jump_up")	
+		animation.play("jump_up")
+		target_position = Vector2(
+			position.x, 
+			position.y - level_tile_size.y * jump_length
+		)
 	elif direction == Vector2.LEFT:
-		var target_position = position.x - level_tile_size.x
-		if target_position > (-level_width / 2) + (level_tile_size.x / 3):
-			tween.tween_property(self, "position:x", target_position, animation_time)
-			animation.play("left")
+		animation.play("left")
+		target_position = Vector2(
+			position.x - level_tile_size.x, 
+			position.y - move_speed * animation.current_animation_length
+		)
+		@warning_ignore("integer_division")
+		var min_x = (-level_width / 2) + (level_tile_size.x / 3)
+		if target_position.x <= min_x: 
+			return null
 	elif direction == Vector2.RIGHT:
-		var target_position = position.x + level_tile_size.x
-		if target_position < (level_width / 2) - (level_tile_size.x / 3):
-			tween.tween_property(self, "position:x", target_position, animation_time)
-			animation.play("right")
+		animation.play("right")
+		target_position = Vector2(
+			position.x + level_tile_size.x, 
+			position.y - move_speed * animation.current_animation_length
+		) 
+		@warning_ignore("integer_division")
+		var max_x = (level_width / 2) - (level_tile_size.x / 3)
+		if target_position.x >= max_x:
+			return null
+	else:
+		return null
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", target_position, animation.current_animation_length)
+	tween.finished.connect(_end_move)
+	return tween
+
+
+func _end_move():
+	if next_move:
+		move(next_move)
+		next_move = Vector2.ZERO
+	else:
+		movement_tween = null
 
 
 ## Прыжок с парашютом по окончании уровня и если health = 0
