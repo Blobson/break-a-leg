07175dc0a74e4599184517ce39d89fd5e8177980
VasyaLel <127948581+VasyaLel@users.noreply.github.com>
2023-10-24 23:19:02 +0300

    #138 сердечки здоровья с анимацией и коррекция UI

--- ui/level_ui/health_indicator/heart.gd
+++ ui/level_ui/health_indicator/heart.gd
@@ -1,14 +1,33 @@
-class_name Heart extends MarginContainer
+class_name Heart extends TextureRect
 
+var texture_heart
 enum State {FULL, BROKEN, EMPTY}
 var state := State.FULL:
 	set(new_state):
-		if new_state == State.EMPTY:
-			queue_free()
-		elif new_state == State.FULL:
-			$TextureRect.texture.set_current_frame(0)
-		elif new_state == State.BROKEN:
-			$TextureRect.texture.set_current_frame(1)
-		state = new_state
+		if state != new_state:
+			_tween(new_state, state)
+			state = new_state
+
+
 func _ready():
-	$TextureRect.texture = $TextureRect.texture.duplicate(true)
+	texture = texture.duplicate(true)
+	texture_heart = texture
+	
+func _tween(new_state, state):
+	var tween = create_tween()
+	if new_state == State.BROKEN and state == State.FULL:
+		tween.tween_property(texture_heart, "current_frame", 4, 1).from(0)
+	elif new_state == State.EMPTY and state == State.BROKEN:
+		tween.tween_property(texture_heart, "current_frame", 8, 1).from(4)
+		tween.finished.connect(_tween_finished)
+	elif new_state == State.EMPTY and state == State.FULL:
+		tween.tween_property(texture_heart, "current_frame", 13, 1).from(9)
+		tween.finished.connect(_tween_finished)
+	elif new_state == State.FULL:
+		texture.set_current_frame(0)
+	elif new_state == State.FULL:
+		texture_heart.current_frame = 0
+		
+func _tween_finished():
+	get_parent().remove_child(self)
+	queue_free()
