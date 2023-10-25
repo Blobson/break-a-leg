bb742ff27cdac383bccab0352a45bbf827cdaeb5
VasyaLel <127948581+VasyaLel@users.noreply.github.com>
2023-10-25 20:36:48 +0300

    #138 сердечки здоровья с анимацией и коррекция UI (#144)
    * #138 сердечки здоровья с анимацией и коррекция UI
    
    * #138 Сердечки здоровья с анимацией и UI
    
    * Update heart.gd

--- ui/level_ui/health_indicator/heart_indicator.gd
+++ ui/level_ui/health_indicator/heart_indicator.gd
@@ -6,20 +6,27 @@ const HEALTH_PER_HEART = 10
 func _init():
 	Game.health_updated.connect(_health_change)
 
-func _health_change(health, _old_value):
+func _health_change(health, old_value):
 	var needed_hearts = ceil(health / float(HEALTH_PER_HEART))
-	var existing_hearts = get_child_count()
+	var existing_hearts = 0
+	for heart in get_children():
+		if heart.state != Heart.State.EMPTY:
+			existing_hearts += 1
 	# Удаляем лишние сердца
 	while existing_hearts > needed_hearts:
-		var heart_node = get_child(0)
-		remove_child(heart_node)
-		heart_node.queue_free()
+		get_child(0).state = Heart.State.EMPTY
 		existing_hearts -= 1
 	# Добавляем недостающие сердца
 	while existing_hearts < needed_hearts:
 		var heart_node = heart_scene.instantiate()
 		add_child(heart_node)
+		get_child(0).state = Heart.State.FULL
 		existing_hearts += 1
 	# У крайнего сердца проставляем State Broken
 	if health % HEALTH_PER_HEART != 0 and existing_hearts > 0:
-		get_child(0).state = Heart.State.BROKEN
+		var broken_heart = null
+		for heart in get_children():
+			if heart.state != Heart.State.EMPTY:
+				broken_heart = heart
+				break
+		broken_heart.state = Heart.State.BROKEN
