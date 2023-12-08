2872efee31238857a32d67a4445a91e05225f1af
Svedmit <127945929+Svedmit@users.noreply.github.com>
2023-12-08 21:04:41 +0300

    #185 Universal recovery animation
    Universal recovery animation
    Ability button fix
    /close #185

--- characters/courier/effects/damage_effect.gd
+++ characters/courier/effects/damage_effect.gd
@@ -2,7 +2,18 @@ class_name DamageEffect extends Node
 
 
 signal finished(courier: Courier)
+var speed_recovery_time: float = 1.
+
+
+func _init():
+	finished.connect(recover)
 
 
 func apply(courier: Courier):
+	courier.enable_slowdown()
 	finished.emit(courier)
+
+
+func recover(courier: Courier):
+	courier.player.play("recovery")
+	courier.recover_speed(speed_recovery_time)
