ecce85362d977697a0c301d0f15109b66b4fc237
Bartolemy12 <olg50337723@gmail.com>
2023-12-10 00:48:00 +0300

    #220 Прикрутить анимацию "бабка трясёт ковром" раз в N секунд

---
+++ regions/residential_region/static_obstacles/grandma/grandma.gd
@@ -0,0 +1,26 @@
+class_name Grandma extends BaseStaticObstacle
+
+
+func _ready():
+	super()
+	$Sprite2D.flip_h = randi_range(0, 1)
+	$ActivateTimer.timeout.connect(_on_activate_timer_timeout)
+	$DustDamageArea.body_entered.connect(_on_damage_area_body_entered)
+
+
+func _on_activate_timer_timeout():
+	player.play('grandma_is_shaking_the_rug')
+
+
+func _on_body_entered(body: Node):
+	super(body)
+	$DustDamageArea.visible = false
+
+
+func _on_damage_area_body_entered(body: Node):
+	var courier = body if body is Courier else body.get_parent()
+	if not courier is Courier:
+		return
+
+	courier.take_damage(damage, null)
+
