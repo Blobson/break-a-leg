d99976c47746bcfff0a1da4abc7e3e06a15c06e5
Svedmit <127945929+Svedmit@users.noreply.github.com>
2023-12-27 22:19:42 +0300

    #210 Indicator image update
    Update health indicator
    Update energy indicator
    /close 210

--- ui/level_ui/health_indicator/heart.gd
+++ ui/level_ui/health_indicator/heart.gd
@@ -1,5 +1,7 @@
 class_name Heart extends TextureRect
 
+const ANIMATION_TIME: float = 0.25
+
 enum State {FULL, BROKEN, EMPTY}
 
 var texture_heart
@@ -18,12 +20,12 @@ func _ready():
 func _tween(new_state, old_state):
 	var tween = create_tween()
 	if new_state == State.BROKEN and old_state == State.FULL:
-		tween.tween_property(texture_heart, "current_frame", 4, 1).from(0)
+		tween.tween_property(texture_heart, "current_frame", 2, ANIMATION_TIME).from(0)
 	elif new_state == State.EMPTY and old_state == State.BROKEN:
-		tween.tween_property(texture_heart, "current_frame", 8, 1).from(4)
+		tween.tween_property(texture_heart, "current_frame", 4, ANIMATION_TIME).from(2)
 		tween.finished.connect(_tween_finished)
 	elif new_state == State.EMPTY and old_state == State.FULL:
-		tween.tween_property(texture_heart, "current_frame", 13, 1).from(9)
+		tween.tween_property(texture_heart, "current_frame", 4, ANIMATION_TIME).from(6)
 		tween.finished.connect(_tween_finished)
 	elif new_state == State.FULL:
 		texture_heart.current_frame = 0
