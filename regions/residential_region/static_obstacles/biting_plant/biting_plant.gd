f0dbf0e004d4a674120de69f17d7ce1e5e06a3c6
Vladimir Klimov <blobson@gmail.com>
2023-12-30 01:10:46 +0300

    #282 Plant bites the courier (#283)

--- regions/residential_region/static_obstacles/biting_plant/biting_plant.gd
+++ regions/residential_region/static_obstacles/biting_plant/biting_plant.gd
@@ -1,5 +1,7 @@
 class_name BitingPlant extends BaseStaticObstacle
 
+const TURN_GAP = 10
+
 var courier_nearby: Courier
 
 func _ready():
@@ -7,7 +9,7 @@ func _ready():
 	$Sprite2D.flip_h = randi_range(0, 1) == 0
 	$ProximityZone.body_entered.connect(_on_body_entered_proximity_zone)
 	$ProximityZone.body_exited.connect(_on_body_exited_proximity_zone)
-
+	$BiteZone.body_entered.connect(_on_body_entered_bite_zone)
 
 func _physics_process(_delta):
 	if courier_nearby:
@@ -17,13 +19,7 @@ func _physics_process(_delta):
 func _on_body_entered_proximity_zone(body: Node):
 	if not is_instance_of(body, Courier):
 		return
-	
-	
 	courier_nearby = body
-	if body.global_position.x > self.global_position.x and $Sprite2D.flip_h != true:
-		$AnimationPlayer.play('turn_right')
-	elif body.global_position.x < self.global_position.x and $Sprite2D.flip_h == true:
-		$AnimationPlayer.play('turn_right', 0.5, 0.5, true)   #Анимация проигрывается с её конца => поворот цветка справа налево
 	_rotate_to_target(body)
 
 
@@ -31,3 +27,32 @@ func _on_body_exited_proximity_zone(body: Node):
 	if body == courier_nearby:
 		courier_nearby = null
 
+
+func _on_damage_apply(courier: Courier):
+	if courier.invulnerability:
+		return
+	$ProximityZone.set_deferred("monitoring", false)
+	$Area2D.set_deferred("monitoring", false)
+	if sprite.flip_h:
+		player.play("bite_right")
+	else:
+		player.play("bite_left")
+
+
+func _on_body_entered_bite_zone(body: Node):
+	if not body is Courier:
+		return
+	
+	$BiteZone.set_deferred("monitoring", false)
+	var damage_effect = PlantBite.new()
+	damage_effect.position = $BiteZone.global_position
+	body.take_damage(damage, damage_effect)
+
+
+func _rotate_to_target(body):
+	if $AnimationPlayer.is_playing() and $AnimationPlayer.current_animation == 'turn_right':
+		return
+	if body.global_position.x > global_position.x + TURN_GAP and not $Sprite2D.flip_h:
+		$AnimationPlayer.play('turn_right')
+	elif body.global_position.x < global_position.x - TURN_GAP and $Sprite2D.flip_h:
+		$AnimationPlayer.play_backwards('turn_right')
