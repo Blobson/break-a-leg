413c4fa3cd17f43df1713f98189ee4065fdf4ca8
Vladimir Klimov <blobson@gmail.com>
2024-01-05 22:57:59 +0300

    #299 Fix jump on windows and conditioners not breaking them

--- regions/residential_region/static_obstacles/conditioner/conditioner.gd
+++ regions/residential_region/static_obstacles/conditioner/conditioner.gd
@@ -2,15 +2,12 @@ extends BaseStaticObstacle
 
 
 func _ready():
-	$AnimationPlayer.play("idle")
-	$Area2D.body_entered.connect(_on_body_entered)
+	super()
 	$SoundFX1.set_pitch_scale(randf_range(0.9, 1.2))
 	$SoundFX1.play()
 
 
 func _on_damage_apply(courier: Courier):
-	if courier.is_jumping():
-		return
 	super(courier)
 	$SoundFX2.play()
 	$SoundFX1._set_playing(false)
