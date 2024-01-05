7297946cc23331979922fbddf2fa350965d915ca
Vladimir Klimov <blobson@gmail.com>
2024-01-05 22:57:59 +0300

    #299 Fix jump on windows and conditioners not breaking them

--- regions/common/base_static_obstacle/base_static_obstacle.gd
+++ regions/common/base_static_obstacle/base_static_obstacle.gd
@@ -3,18 +3,19 @@ class_name BaseStaticObstacle extends Node2D
 @export var damage: int = 5
 @export var flippable: bool = false
 @export var reusable: bool = true
+@export var can_be_jumped_over: bool = true
 
 @onready var player = $AnimationPlayer
 @onready var sprite = $Sprite2D
 
-enum ObstacleState {IDLE, ACTIVATED}
-var state: ObstacleState = ObstacleState.IDLE
+var jumping_courier: Courier = null
 
 
 func _ready():
 	if $AnimationPlayer.has_animation("idle"):
 		$AnimationPlayer.play("idle")
 	$Area2D.body_entered.connect(_on_body_entered)
+	$Area2D.body_exited.connect(_on_body_exited)
 
 
 func get_damage_effect() -> DamageEffect:
@@ -22,21 +23,29 @@ func get_damage_effect() -> DamageEffect:
 
 
 func _on_body_entered(body: Node):
-	if state != ObstacleState.IDLE:
-		return
-	
 	var courier = body if body is Courier else body.get_parent()
 	if not courier is Courier:
 		return
-	
-	# смена state на активированную
-	state = ObstacleState.ACTIVATED
+		
+	if can_be_jumped_over and courier.is_jumping():
+		jumping_courier = courier
+		return
 	
 	# Если препятствие может поворачиваться, то используется _rotate_to_target()
 	if flippable == true:
 		_rotate_to_target(courier)
 	
 	_on_damage_apply(courier)
+
+
+func _on_body_exited(body: Node):
+	jumping_courier = null
+
+
+func _process(_delta):
+	if jumping_courier and not jumping_courier.is_jumping():
+		_on_damage_apply(jumping_courier)
+		jumping_courier = null
 
 
 func _on_damage_apply(courier: Courier):
@@ -51,7 +60,6 @@ func _on_damage_apply(courier: Courier):
 
 func _on_damage_done():
 	if reusable:
-		state = ObstacleState.IDLE
 		if player.has_animation("idle"):
 			player.play("idle")
 
--- regions/residential_region/static_obstacles/window/window.gd
+++ regions/residential_region/static_obstacles/window/window.gd
@@ -2,18 +2,8 @@ extends BaseStaticObstacle
 
 
 func _on_damage_apply(courier: Courier):
-	if courier.is_jumping():
-		return
-	
 	play_breaking_sound(randi_range(1,2))
-	
-	courier.take_damage(damage, get_damage_effect())
-
-	if player.has_animation("activate"):
-		player.play("activate")  # анимация активации
-		player.animation_finished.connect(func(a): if a == "activate": _on_damage_done(), CONNECT_ONE_SHOT)
-	else:
-		_on_damage_done()
+	super(courier)
 
 
 func _on_damage_done():
